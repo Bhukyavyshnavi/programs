ff18aa78dc50fa559bd4a7dc352e2a7fc3feba57
VYSHNAVI BHUKYA <20wh1a0538@bvrithyderabad.edu.in>
2022-12-26 18:05:19 +0530

    nested loops

---
+++ nested loops/multiplication_table.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int MIN = 1;
+	int MAX = 10;
+    cout<<"  ";
+	for(int i=1;i<=(MIN, MAX);i++){
+		cout<<i<<" ";
+	}
+	cout<<"\n";
+	for(int i=1;i<=(MIN, MAX);i++){
+		cout<<i<<" ";
+	    for(int j=1;j<=(MIN, MAX);j++){
+			cout<<(i  *  j)<<" ";
+		}
+		cout<<"\n";
+		}
+	}
